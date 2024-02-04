76d3cb4e721c1e67db6059b9c5f64cd8edd4bad2
Lucy Sillars <2767670s@student.gla.ac.uk>
2024-02-04 18:14:23 +0000

    git add * git commit -m "Chapter 6" git push

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 # Create your views here.
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages = Page.objects.order_by('-views')[:5]
+
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
+        'pages': pages,
+    }
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to amke our lives easier.
@@ -14,3 +25,20 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
